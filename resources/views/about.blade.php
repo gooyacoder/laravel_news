34fd6e66eefdde40d0edfc492a85503ced37ab78
gooyacoder <gooyacoder@gmail.com>
2020-04-25 12:19:34 +0430

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>About Us Page</h1>
+<i><?php echo $fName . " " . $lName; ?></i>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/{id}/{name?}', function ($id, $name="codepro") {
+    return "Id : $id, Name : $name";
+})->where('id', '[0-9]+');
+
+Route::get('/world', 'HelloController@hello');
+Route::get('/add', 'AddController@index');
+
+Route::get('/about', ['uses'=>'AboutController@about', 'as'=>'about']);
